cea42b7f02690e1141bc0ef2df6d78a73c7a6d8a
PeacefulToadz <peacefultoadz@gmail.com>
2022-01-10 21:32:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,244 @@
+
+import styles from './Home.module.css'
+function App() {
+  return (
+    <div >
+  
+
+    <main className={styles.main}>
+
+    <div className={styles.banner_links_container} > 
+
+
+  <div className={styles.banner_importants_container} >
+
+    <div> <a><img src="opensea.svg" alt="twitter" width="75px"/></a></div> 
+    <div> <a href="https://discord.gg/peacefultoadz"><img src="discord.svg" alt="twitter" width="50px"/></a></div>  
+    <button className={styles.banner_mint_button} >GO TO MINT PAGE</button>
+     <div> <a href="https://twitter.com/PeacefulToadz"><img src="twitter.svg" alt="twitter" width="50px"/></a></div> 
+     <div><h2>🕒</h2></div> 
+  </div>
+<h5>&middot; Supply: 8888&nbsp; &middot; Price: 0.066 ETH(+gas)&nbsp; &middot; 3 mint per wallet &nbsp; &middot; Pre-sale: 14th January 15:30 EST &nbsp; &middot; Public-sale: 15th January 15:30 EST   </h5>
+
+  </div>
+
+
+
+
+    <div className={styles.cloud_container} >
+  <div className={styles.banner_anim}>
+
+
+<div className={styles.banner_anim_2}>
+
+<div className={styles.banner_container} >
+<div className={styles.banner_image_container} > 
+  <img src="banner.png" alt="Girl in a jacket"/>
+  </div>
+  <img   className={styles.lefty} src="lefty.png" alt="Girl in a jacket" height="100"/>
+  <img   className={styles.righty} src="lefty.png" alt="Girl in a jacket" height="100"/>
+  <img   className={styles.screen} src="vibez.png" alt="Girl in a jacket" height="30"/>
+</div>
+
+</div>
+
+</div>
+</div>
+
+  
+
+  
+  <div className={styles.info_container}>
+<div className={styles.info_area}>
+<div>
+<h3>WHAT'S PEACEFULTOADZ?</h3>
+<p>
+Peaceful Toadz is the Peaceful Groupies & cryptToadz crossover project that aims to bring the best vibes of the nft world together. Created by the best possible blending of the two collections, 8888 unique piece is here to provide the fuel needed to go from swamp to the moon. </p>
+
+</div>
+<div className={styles.info_image}>
+<img src="15.png" alt="nft" height="400px"/>
+</div>
+
+
+</div>
+
+
+  </div>
+
+
+<div className={styles.fly_container}>
+<img src="fly.png" alt="nft" height="40px"/>
+<div><h3>Best Vibes of the nft world together!</h3></div>
+</div>
+
+
+
+
+
+
+
+<div className={styles.showroom_container} >
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#515C48" fill-opacity="1" d="M0,192L48,213.3C96,235,192,277,288,261.3C384,245,480,171,576,144C672,117,768,139,864,170.7C960,203,1056,245,1152,256C1248,267,1344,245,1392,234.7L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg><div className={styles.showroom}>
+
+<img src="1.jpg" alt="nft" height="300px"/>
+<img src="BG1.gif" alt="nft" height="300px"/>
+<img src="4.png" alt="nft" height="300px"/>
+<img src="1.png" alt="nft" height="300px"/>
+<img src="BG10.gif" alt="nft" height="300px"/>
+<img src="story.png" alt="nft" height="300px"/>
+
+
+
+</div>
+
+</div>
+<div className={styles.info_container2}>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#40312B " fill-opacity="1" d="M0,64L6.2,80C12.3,96,25,128,37,154.7C49.2,181,62,203,74,181.3C86.2,160,98,96,111,96C123.1,96,135,160,148,176C160,192,172,160,185,160C196.9,160,209,192,222,197.3C233.8,203,246,181,258,181.3C270.8,181,283,203,295,181.3C307.7,160,320,96,332,64C344.6,32,357,32,369,64C381.5,96,394,160,406,202.7C418.5,245,431,267,443,266.7C455.4,267,468,245,480,208C492.3,171,505,117,517,117.3C529.2,117,542,171,554,197.3C566.2,224,578,224,591,234.7C603.1,245,615,267,628,266.7C640,267,652,245,665,218.7C676.9,192,689,160,702,128C713.8,96,726,64,738,42.7C750.8,21,763,11,775,21.3C787.7,32,800,64,812,106.7C824.6,149,837,203,849,208C861.5,213,874,171,886,154.7C898.5,139,911,149,923,165.3C935.4,181,948,203,960,229.3C972.3,256,985,288,997,277.3C1009.2,267,1022,213,1034,176C1046.2,139,1058,117,1071,128C1083.1,139,1095,181,1108,192C1120,203,1132,181,1145,197.3C1156.9,213,1169,267,1182,256C1193.8,245,1206,171,1218,138.7C1230.8,107,1243,117,1255,112C1267.7,107,1280,85,1292,106.7C1304.6,128,1317,192,1329,229.3C1341.5,267,1354,277,1366,240C1378.5,203,1391,117,1403,96C1415.4,75,1428,117,1434,138.7L1440,160L1440,0L1433.8,0C1427.7,0,1415,0,1403,0C1390.8,0,1378,0,1366,0C1353.8,0,1342,0,1329,0C1316.9,0,1305,0,1292,0C1280,0,1268,0,1255,0C1243.1,0,1231,0,1218,0C1206.2,0,1194,0,1182,0C1169.2,0,1157,0,1145,0C1132.3,0,1120,0,1108,0C1095.4,0,1083,0,1071,0C1058.5,0,1046,0,1034,0C1021.5,0,1009,0,997,0C984.6,0,972,0,960,0C947.7,0,935,0,923,0C910.8,0,898,0,886,0C873.8,0,862,0,849,0C836.9,0,825,0,812,0C800,0,788,0,775,0C763.1,0,751,0,738,0C726.2,0,714,0,702,0C689.2,0,677,0,665,0C652.3,0,640,0,628,0C615.4,0,603,0,591,0C578.5,0,566,0,554,0C541.5,0,529,0,517,0C504.6,0,492,0,480,0C467.7,0,455,0,443,0C430.8,0,418,0,406,0C393.8,0,382,0,369,0C356.9,0,345,0,332,0C320,0,308,0,295,0C283.1,0,271,0,258,0C246.2,0,234,0,222,0C209.2,0,197,0,185,0C172.3,0,160,0,148,0C135.4,0,123,0,111,0C98.5,0,86,0,74,0C61.5,0,49,0,37,0C24.6,0,12,0,6,0L0,0Z"></path></svg>
+<div className={styles.info_second_container}>
+
+<div className={styles.info_area2}>
+<h3>STORY</h3>
+<p>Evil King Gremplin goes to Peaceful Groupie's land in search of the toadz who escaped from him and joined the groupies. He finds a trace of the toadz. Do you think he will find them? You can help them escaping from the Evil King Gremplin!</p>
+<img src="kingstory.jpg" alt="nft" />
+
+</div>
+
+
+</div>
+
+
+  </div>
+
+  <div className={styles.fly_container}>
+
+<div><h3>MEET THE TEAM!</h3></div>
+</div> 
+<div className={styles.team_container}>
+
+
+
+<div >
+<span className={styles.help_tip}>
+  <p>This is the inline help tip! It can contain all kinds of HTML. Style it as you please.<br />
+
+  </p>
+</span>
+<img src="ilkay.png" alt="nft" height="250px"/>
+<h4>Founder</h4>
+<h4>Project Manager</h4>
+<div className={styles.twit}><a href="https://twitter.com/nftartclubtr" ><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>nftartclubtr</a></div>
+
+</div>
+<div>
+<span className={styles.help_tip}>
+  <p>This is the inline help tip! It can contain all kinds of HTML. Style it as you please.<br />
+
+  </p>
+</span>
+<img src="berkay.png" alt="nft" height="250px"/>
+<h4>Community Manager</h4>
+<h4>Coordinator</h4>
+<div className={styles.twit}><a href="https://twitter.com/drthiccfloyd" ><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>drthiccfloyd</a></div>
+
+</div>
+<div>
+<span className={styles.help_tip}>
+  <p>This is the inline help tip! It can contain all kinds of HTML. Style it as you please.<br />
+
+  </p>
+</span>
+<img src="ilker.png" alt="nft" height="250px"/>
+<h4>Artist</h4>
+<h4> &nbsp;</h4>
+<div className={styles.twit}><a href="https://twitter.com/ilkeryuksell"  ><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>ilkeryuksell</a></div>
+
+</div>
+<div >
+<span className={styles.help_tip}>
+  <p>This is the inline help tip! It can contain all kinds of HTML. Style it as you please.<br />
+
+  </p>
+</span>
+<img src="2.png" alt="nft" height="250px"/>
+<h4>Development Lead</h4>
+<h4> &nbsp;</h4>
+<div className={styles.twit}><a href="https://twitter.com/cuneytsenyavas"  ><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>cuneytsenyavas</a></div>
+
+</div>
+<div>
+<span className={styles.help_tip}>
+  <p>This is the inline help tip! It can contain all kinds of HTML. Style it as you please.<br />
+
+  </p>
+</span>
+<img src="tarık.png" alt="nft" height="250px"/>
+<h4>Developer</h4>
+<h4> &nbsp;</h4>
+
+<div className={styles.twit}><a  ><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>tiqdev </a></div>
+
+</div>
+<div>
+<span className={styles.help_tip}>
+  <p>This is the inline help tip! It can contain all kinds of HTML. Style it as you please.<br />
+
+  </p>
+</span>
+<img src="story.png" alt="nft" height="250px"/>
+<h4>Marketing</h4>
+<h4> &nbsp;</h4>
+<div className={styles.twit}><a href="https://twitter.com/ikarussnft"  ><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>ikarussnft</a></div>
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+    </main>
+
+ 
+    <footer className={styles.footer}>
+      <a
+      
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '} <b> Peaceful Toadz . </b>    
+       
+      </a>
+     <a
+       href="https://peacefultoadz.com"
+       target="_blank"
+       rel="noopener noreferrer"
+     >
+       opensea
+     </a>
+
+     <a href="https://twitter.com/PeacefulToadz">
+    twitter
+     </a>
+     <a href="https://discord.gg/peacefultoadz">
+    
+       discord
+     </a>
+     <a
+       href="https://peacefultoadz.com"
+       target="_blank"
+       rel="noopener noreferrer"
+     >
+       minting page
+     </a>
+     
+    </footer>
+
+  </div>
+  );
+}
+
+export default App;
