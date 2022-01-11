f54be052f2d6e533f303f093aa6a070e810f04db
PeacefulToadz <peacefultoadz@gmail.com>
2022-01-11 18:20:25 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div> <a href="https://discord.gg/peacefultoadz"><img src="discord.svg" alt="twitter" width="50px"/></a></div>  
     <button className={styles.banner_mint_button} >GO TO MINT PAGE</button>
      <div> <a href="https://twitter.com/PeacefulToadz"><img src="twitter.svg" alt="twitter" width="50px"/></a></div> 
-     <div><h2>🕒</h2></div> 
+     <div><a href="https://dateful.com/time-zone-converter"><h2>🕒</h2> </a></div> 
   </div>
 <h5>&middot; Supply: 8888&nbsp; &middot; Price: 0.066 ETH(+gas)&nbsp; &middot; 3 mint per wallet &nbsp; &middot; Pre-sale: 14th January 15:30 EST &nbsp; &middot; Public-sale: 15th January 15:30 EST   </h5>
 
@@ -57,7 +57,7 @@ Peaceful Toadz is the Peaceful Groupies & cryptToadz crossover project that aims
 
 </div>
 <div className={styles.info_image}>
-<img src="15.png" alt="nft" height="400px"/>
+<img src="15.png" alt="nft" />
 </div>
 
 
@@ -172,7 +172,7 @@ Peaceful Toadz is the Peaceful Groupies & cryptToadz crossover project that aims
 
   </p>
 </span>
-<img src="tarık.png" alt="nft" height="250px"/>
+<img src="tarik.png" alt="nft" height="250px"/>
 <h4>Developer</h4>
 <h4> &nbsp;</h4>
 
@@ -185,8 +185,8 @@ Peaceful Toadz is the Peaceful Groupies & cryptToadz crossover project that aims
 
   </p>
 </span>
-<img src="story.png" alt="nft" height="250px"/>
-<h4>Marketing</h4>
+<img src="berke.png" alt="nft" height="250px"/>
+<h4>PR</h4>
 <h4> &nbsp;</h4>
 <div className={styles.twit}><a href="https://twitter.com/ikarussnft"  ><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>ikarussnft</a></div>
 
